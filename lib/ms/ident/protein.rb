eee8ca7bdff6efded1a38d8a5b46f90e212d89a0
John Prince <jtprince@gmail.com>
2011-03-28 22:00:48 -0600

    peptide_hits_to protein groups working well now

--- lib/ms/ident/protein.rb
+++ lib/ms/ident/protein.rb
@@ -14,19 +14,29 @@ module Ms::Ident::Protein
     reference.split(/[\s\r]/)[0]
   end
 
+  PRIORITIZE_PROTEINS = lambda do |protein_group_and_peptide_hits|
+    peptide_hits = protein_group_and_peptide_hits.last
+    num_uniq_aaseqs = peptide_hits.map {|hit| hit.aaseq }.uniq.size
+    num_uniq_aaseqs_at_z = peptide_hits.map {|hit| [hit.aaseq, hit.charge] }.uniq.size
+    [num_uniq_aaseqs, num_uniq_aaseqs_at_z, peptide_hits.size]
+  end
+
+
   module_function
-  # greedy algorithm to map a set of peptide_hits to protein groups.
-  # each peptide hit should respond to :aaseq, :charge, :proteins if a block
-  # is given, yields a set of peptide hits and expects a metric or array to
-  # sort by for creating greedy protein groups (the greediest proteins should
-  # sort to the back of the array).  if no block is given, the groups are
-  # sorted by [# uniq aaseqs, # uniq aaseq+charge, # peptide_hits].
-  def peptide_hits_to_protein_groups(peptide_hits, &sort_peptide_sets_by)
-    sort_peptide_sets_by ||= lambda {|peptide_hits|
-      num_uniq_aaseqs = peptide_hits.map {|hit| hit.aaseq }.uniq.size
-      num_uniq_aaseqs_at_z = peptide_hits.map {|hit| [hit.aaseq, hit.charge] }.uniq.size
-      [num_uniq_aaseqs, num_uniq_aaseqs_at_z, peptide_hits.size]
-    }
+  # greedy algorithm to map a set of peptide_hits to protein groups.  each
+  # peptide hit should respond to :aaseq, :charge, :proteins if a block is
+  # given, yields a single argument: a doublet of protein_group and peptide
+  # set.  It expects a metric or array to sort by for creating greedy protein
+  # groups (the greediest proteins should sort to the back of the array).  if
+  # no block is given, the groups are sorted by [# uniq aaseqs, # uniq
+  # aaseq+charge, # peptide_hits] (see PRIORITIZE_PROTEINS).  Sets of
+  # peptide_hits and the objects returned by peptide_hit#proteins are used as
+  # hash keys.  As long as each peptide hit has a unique signature (like an
+  # id) then any object will work.  If they are Struct objects, you might
+  # consider redefining the #hash method to be object_id for performance and
+  # accuracy.
+  def peptide_hits_to_protein_groups(peptide_hits, &sort_by)
+    sort_by ||= PRIORITIZE_PROTEINS
     # note to self: I wrote this in 2011, so I think I know what I'm doing now
     protein_to_peptides = Hash.new {|h,k| h[k] = Set.new }
     peptide_hits.each do |peptide_hit|
@@ -38,17 +48,21 @@ module Ms::Ident::Protein
     protein_to_peptides.each do |protein, peptide_set|
       peptides_to_protein_group[peptide_set] << protein
     end
-    sorted_peptide_sets = peptides_to_protein_group.keys.sort_by(&sort_peptide_sets_by)
+    protein_group_to_peptides = peptides_to_protein_group.invert
+    greedy_first = protein_group_to_peptides.sort_by(&sort_by).reverse
     accounted_for = Set.new
     surviving_protein_groups = []
-    peptide_sets_with_unaccounted_peptides = sorted_peptide_sets.reverse.select do |peptide_set|
-      peptide_set.any? do |peptide_hit| 
-        # has an unaccounted for peptide?
-        accounted_for.include?(peptide_hit) ? false : accounted_for.add(peptide_hit)
+    # we are discarding the subsumed sets, but we could get them with
+    # partition
+    greedy_first.select do |group, peptide_set|
+      has_an_unaccounted_peptide = false
+      peptide_set.each do |peptide_hit| 
+        unless accounted_for.include?(peptide_hit) 
+          has_an_unaccounted_peptide = true
+          accounted_for.add(peptide_hit)
+        end
       end
-    end
-    peptide_sets_with_unaccounted_peptides.map do |peptide_set|
-      [peptides_to_protein_group[peptide_set], peptide_set]
+      has_an_unaccounted_peptide
     end
   end
 
--- spec/ms/ident/protein_spec.rb
+++ spec/ms/ident/protein_spec.rb
@@ -3,14 +3,16 @@ require 'spec_helper'
 require 'ms/ident/protein'
 
 PeptideHit = Struct.new(:aaseq, :charge, :proteins) do
-  def inspect
+  def inspect # easier to read output
     "<PeptideHit aaseq=#{self.aaseq} charge=#{self.charge} proteins(ids)=#{self.proteins.map(&:id).join(',')}>"
   end
+  def hash ; self.object_id end
 end
 ProteinHit = Struct.new(:id) do
-  def inspect
+  def inspect # easier to read output
     "<Prt #{self.id}>"
   end
+  def hash ; self.object_id end
 end
 
 describe 'creating minimal protein groups from peptide hits' do
@@ -21,24 +23,47 @@ describe 'creating minimal protein groups from peptide hits' do
       ['DDEEFFGG', 3],
       ['HIYA', 2],
     ].map {|ar| PeptideHit.new(ar[0], ar[1], []) }
-    { 'big_guy' => @pep_hits,
+    @prot_hits_hash = { 
+      'big_guy' => @pep_hits,
       'little_guy' => [@pep_hits.last],
-      'medium_guy' => @pep_hits[0,4],
+      'medium_guy1' => @pep_hits[0,4],
+      'medium_guy2' => @pep_hits[0,4],
       'subsumed_by_medium' => @pep_hits[2,2],
-    }.each do |id, peptides|
-      peptides.each {|pep| pep.proteins << ProteinHit.new(id) }
-    end
+    }
+    @prot_hits = @prot_hits_hash.keys.map {|id| ProteinHit.new(id) }
   end
-  
-  it 'works' do
+
+  it 'is a greedy algorithm' do
+    @prot_hits.each {|prthit| @prot_hits_hash[prthit.id].each {|pep| pep.proteins << prthit } }
+    # big_guy has all the peptides, so it takes them all
     reply = Ms::Ident::Protein.peptide_hits_to_protein_groups(@pep_hits)
-    reply.each do |group, peptide_hits|
-      puts "GROUP / PEPS: "
-      p group
-      p peptide_hits
-    end
-    1.is 1
+    reply.first.size.is 2 # the group and the peptide set
+    reply.first.first.size.is 1 # the group
+    reply.first.first.first.id.is 'big_guy'
   end
 
+  it 'removes proteins accounted for only as little pieces of larger proteins' do
+    @prot_hits[1..-1].each {|prthit| @prot_hits_hash[prthit.id].each {|pep| pep.proteins << prthit } }
+    reply = Ms::Ident::Protein.peptide_hits_to_protein_groups(@pep_hits)
+    # no subsumed_by_medium
+    reply.map(&:first).any? {|protein_list| protein_list.any? {|v| v.id == 'subsumed_by_medium' }}.is false
+  end
 
+  it 'allows alternate sorting algorithms for greediness' do
+    @prot_hits.each {|prthit| @prot_hits_hash[prthit.id].each {|pep| pep.proteins << prthit } }
+    reply = Ms::Ident::Protein.peptide_hits_to_protein_groups(@pep_hits) do |prot_and_peptide_hits|
+      # deliberate using a counterintuitive sorting method to give little guys
+      # a chance
+      -prot_and_peptide_hits.last.size
+    end
+    # because the little proteins are given priority, they 'survive'. Bigger
+    # proteins may also survive if they have at least one unique peptide
+    # to add to the mix.  This demonstrates how proteins can be weighted in
+    # different ways based on their peptide hits.
+    seen = []
+    reply.each {|pair| pair.first.each {|prot| seen << prot.id } }
+    # big guy is completely accounted for in the now prioritized little guy
+    # and medium guys, etc.
+    seen.sort.is @prot_hits_hash.keys[1..-1].sort
+  end
 end
