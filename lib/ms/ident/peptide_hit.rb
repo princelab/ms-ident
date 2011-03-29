826489dd06a5d4b1ac8938ad56332b8c60108689
John Prince <jtprince@gmail.com>
2011-03-29 17:32:05 -0600

    progress

---
+++ lib/ms/ident/peptide_hit.rb
@@ -0,0 +1,8 @@
+module Ms ; end
+module Ms::Ident ; end
+
+class Ms::Ident::PeptideHit
+  attr_accessor :aaseq
+  attr_accessor :charge
+  attr_accessor :proteins
+end
---
+++ lib/ms/ident/peptide_hit/qvalue.rb
@@ -0,0 +1,55 @@
+require 'ms/ident/peptide_hit'
+
+module Ms ; end
+module Ms::Ident ; end
+
+class Ms::Ident::PeptideHit
+  module Qvalue
+    attr_accessor :qvalue
+    FILE_EXTENSION = '.phq.tsv'
+    FILE_DELIMITER = "\t"
+    HEADER = %w(aaseq charge qvalue)
+
+    class << self
+
+      # writes to the file, adding an extension
+      def to_phq(base, hits, qvalues=nil)
+        to_file(base + FILE_EXTENSION, hits)
+      end
+
+      # writes the peptide hits to a phq.tsv file. qvalues is a parallel array
+      # to hits that can provide qvalues if not inherent to the hits
+      # returns the filename.
+      def to_file(filename, hits, qvalues=[])
+        File.open(filename,'w') do |out|
+          out.puts HEADER.join(FILE_DELIMITER)
+          hits.zip(qvalues) do |hit, qvalue|
+            out.puts [hit.aaseq, hit.charge, qvalue || hit.qvalue].join(FILE_DELIMITER)
+          end
+        end
+        filename
+      end
+
+      # returns an array of PeptideHit objects from a phq.tsv
+      def from_file(filename)
+        peptide_hits = []
+        File.open(filename) do |io|
+          header = io.readline.chomp.split(FILE_DELIMITER)
+          raise "bad headers" unless header == HEADER 
+          io.each do |line|
+            line.chomp!
+            (aaseq, charge, qvalue) = line.split(FILE_DELIMITER)
+            ph = PeptideHit.new
+            ph.aaseq = aaseq ; ph.charge = charge ; ph.qvalue = qvalue
+            peptide_hits << ph
+          end
+        end
+        peptide_hits
+      end
+
+      alias_method :from_phq, :from_file
+
+    end
+  end # Qvalue
+  include Qvalue
+end # Peptide Hit
